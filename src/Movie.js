3ff37a317f3201a99f92c821c4aa5e96188b9b71
Tu Anh Huynh <tuanhlyhuynh@gmail.com>
2020-12-09 07:54:03 -0500

    [Movie] some minor fixes

--- src/Movie.js
+++ src/Movie.js
@@ -2,7 +2,7 @@ function Movie(props) {
     console.log(props);
     return (
         <div>
-            <h1>{props.title}</h1>
+            <h1>Lord of the Rings: {props.title}</h1>
             <p>{props.hours}:{props.minutes}</p>
         </div>
     )
